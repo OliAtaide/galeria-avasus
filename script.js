1c23a962760efdbe2a793b37205a3e3ae7f32ef9
Filipe de Oliveira de Ataíde <filipe.ataide@lais.huol.ufrn.br>
2022-07-20 08:27:02 -0300

    Cabecalho

--- script.js
+++ script.js
@@ -2,34 +2,43 @@ let obras = [
     {
         autor_foto: 'images/autor.png',
         autor_texto: 'Bevenuto Cellini nasceu em 1500 e foi um dos maiores escultores renascentistas, também reconhecido por seu talento como ourives, escritor e músico. Era um gênio nas artes, mas tinha um comportamento controverso, se envolvendo em vários escândalos sexuais e homicídios, tendo relatado os fatos em sua autobiografia, escrita por ele mesmo (CELLINI, 1902). Aos 29 anos de idade, ele contraiu sífilis e, ao entrar na segunda fase da doença, foi orientado a fazer uso de mercúrio, uma das opções de tratamento na época. Por temer os efeitos colaterais, ele recusou a medicação, dando preferência a aplicação de sanguessugas e loções. Ao evoluir para a fase terciária da sífilis, surgiu um rumor de que Celline estaria em estado terminal. Nesse período, envolveu-se em uma compra desvantajosa, e as pessoas com as quais ele negociou, tentaram envenená-lo, adicionando mercúrio a sua alimentação, porém, a dose administrada não foi suficiente para matá-lo, mas sim para curá-lo da sífilis (WOLF, 2005).',
+        obra_titulo: 'A sífilis, Benvenuto Cellini e “Perseus  com a Cabeça de Medusa”',
         obra_foto: 'images/obra1.png',
         obra_texto: 'Em resposta a tentativa de assassinato contra a sua vida e a experiência vivenciada durante o período de adoecimento pela sífilis, Cellini esculpiu em bronze uma obra-prima, intitulada “Perseus com a Cabeça de Medusa”. Essa escultura além de representar a vitória de Celline sobre os seus inimigos ou sobre a enfermidade que o atormentou, também expressa a sua genialidade, por meio da capacidade representativa, da beleza estética exibida em cada detalhe e do poder de transmissão de seu próprio sentimento interior enquanto criador. Essa obra resistiu aos séculos e pode ser encontrada na Piazza della Signorina, em Florença, na Itália (WOLF, 2005).'
     },
     {
         autor_foto: 'images/autor2.jpg',
         autor_texto: 'François Marie Arouet nasceu na França, mais precisamente em Paris, no ano de 1694. Pertencente a uma família burguesa, adotou o pseudônimo de Voltaire, e foi um dos maiores expoentes do Iluminismo, junto com Russeau e Montesquieu. Voltaire foi um proeminente escritor e filósofo, além de dramaturgo, poeta, ensaísta e historiador, tendo estudado no Collège Louis-le Grand, em sua cidade Natal, onde iniciou o curso de Direito, porém não chegou a finalizar. Era conhecido por seu temperamento forte e suas ideias revolucionárias, tendo frequentado a “Société du Temple”, uma associação onde se reuniam de libertinos a livres pensadores (BARTHES, 1964).',
+        obra_titulo: 'A sífilis, Voltaire e “O homem  dos  quarenta  escudos”',
         obra_foto: 'images/obra2.png',
         obra_texto: 'Essa obra literária apresenta-se como uma crítica sociopolítica, na qual são abordados diversos temas polêmicos no século XVI, e de uma forma contundente e mordaz, ele passa a investir contra autoridades, sejam políticas ou religiosas, e contra os médicos (BARTHES, 1964). Este capítulo é totalmente dedicado à sífilis, e evidencia uma conversa entre o homem dos quarenta escudos e o cirurgião-mor. Eles discorrem sobre a forma como a sífilis chegou a Europa, como se disseminou, e atingiu de pobres a burgueses, cultos e incultos, nobres e clérigos, além de abordar aspectos relacionados à sintomatologia, e transmissão, e também explorar o fato de haver sido disseminada por meio dos soldados envolvidos com as guerras e a prostituição. <br>“O homem dos quarenta escudos morava num pequeno cantão, onde fazia uns cento e cinquenta anos que não acampavam soldados. Os costumes, naquele desconhecido rincão, eram mais puros do que o ar que o banha. Não se sabia que alhures pudesse o amor ser infeccionado de um veneno destrutivo, que as gerações fossem atacadas no seu germe, e que a natureza, contradizendo-se a si mesma, pudesse tornar a carícia horrível e o prazer medonho; entregavam-se ao amor com a segurança da inocência. Chegaram tropas, e tudo mudou. Dois tenentes, o esmoler do regimento, um cabo e um recruta proveniente do seminário bastaram para envenenar doze aldeias em menos de três meses. Duas primas do homem dos quarenta escudos viram-se cobertas de pústulas; caíram-lhes os lindos cabelos; a sua voz tornou-se rouca; as pálpebras de seus olhos fixos e apagados tomaram uma cor lívida, e não mais se fecharam para permitir repouso aos membros deslocados, que uma cárie secreta começava a roer como aos do árabe Jó, embora Jó jamais tivesse tido semelhante doença. (Voltaire, 1768, p.91)”'
     },
     {
         autor_foto: 'images/autor3.jpg',
         autor_texto: 'Ramon Casas i Carbó foi um pintor e caricaturista espanhol, nascido em 1886, e um dos expoentes do modernismo Catalão devido as suas contribuições na pintura e nas artes gráficas. Em 1900, Casas criou um cartaz abordando o tema da sífilis para “promover o Sanatório de Dr. Abreu, localizado em Bonanova, e que oferecia tratamento especializado para sífilis (HERNÁNDEZ, 2016).',
+        obra_titulo: ' A sífilis, Ramon Casas e o cartaz do sanatório para sifilíticos',
         obra_foto: 'images/obra3.png',
         obra_texto: 'Em 1900, Casas criou um cartaz abordando o tema da sífilis para “promover o Sanatório de Dr. Abreu, localizado em Bonanova, e que oferecia tratamento especializado para sífilis (HERNÁNDEZ, 2016).'
     },
     {
         autor_foto: 'images/autor4.jpg',
         autor_texto: 'Nasceu na Noruega, em Oslo, no final do século XIX, e é considerado o precursor do expressionismo alemão, um movimento de contraponto às normas burguesas, que propunha um novo padrão estético, no qual a realidade se apresentava de uma forma trágica e crítica em relação à ordem social dominante por meio da representação de imagens distorcidas, deformadas carregadas de sentimentos de angústia, tristeza e medo. Uma de suas obras “O grito” é considerada como a maior representação do expressionismo. Munch perdeu a mãe e uma irmã, ainda jovem, em seguida, sua outra irmã foi diagnosticada com problemas mentais, seu avô foi diagnosticado com sífilis, e o próprio Munch foi uma criança enfermiça e chegou a procurar tratamento psiquiátrico quando adulto, por apresentar um quadro de perturbação e conflitos, além de diagnóstico de ansiedade, depressão e transtorno bipolar (AZEEM, 2015).',
+        obra_titulo: 'A sífilis, Edvard Munch e a “Herança” ',
         obra_foto: 'images/obra4.jpg',
         obra_texto: 'Vamos conhecer um quadro “que foi exposto no “Salão dos independentes”, em Paris no ano de 1903, intitulado inicialmente como “Mãe” e, depois, renomeado como “Herança”. Nessa obra Munch retrata uma mãe sifilítica e seu filho enfermo com sífilis congênita. A cena, desesperadora e triste, nos remete a um aspecto importante, que é a forma distinta como Munch retrata mãe e filho, e a exposição dos sinais clínicos, resultado de um provável estudo de Munch acerca da sífilis congênita, durante visita ao Hôpital Saint-Louis na cidade de Paris, onde conheceu uma coleção em cera usada para o estudo de anatomia, incluindo uma peça de um recém-nascido com sífilis congênita, e na mesma ocasião, viu uma cena real, na qual uma mãe chorava ao lado de seu filho com sífilis (PERCIACCANTE; CORALLI, 2018). O quadro sofreu várias críticas, principalmente pelo fato de retratar uma infecção sexualmente transmissível, tabu na época. O quadro foi pintado, em torno de 1887 a 1889, e foi exposto em 1903.'
     },
     {
         autor_foto: 'images/autor5.jpg',
         autor_texto: 'Guiseppe Danielle Sargent, mais conhecido como Joseph Sargent, nasceu em 22 de julho de 1925 em Jersey City, Nova Jersey, EUA. Foi um diretor americano, que atuou em televisão e cinema e dirigiu, entre outras obras os filmes Tubarão 4: A Vingança (1987), Pesadelos Diabólicos (1983) e Jornada nas Estrelas (1966).',
+        obra_titulo: 'A sífilis, Joseph Sargent e Miss Evers’ Boys ',
         obra_foto: 'images/obra5.png',
         obra_texto: '"Em 1996, Joseph Sargent dirigiu a obra ficcional “Miss Evers’ Boys”. Essa obra se baseia na história real sobre um projeto de pesquisa realizado nos Estados Unidos, em Alabama, na cidade rural de Tuskegee, subsidiado pelo departamento de saúde pública do governo americano, entre os anos de 1932 e 1972”, e denominado “Tuskegee Study of Untreated Syphilis in the Negro Male”. Muito do significado do filme se baseia na ausência de princípios éticos no projeto de pesquisa, que, deliberadamente, apenas dispensou placebo para os pacientes portadores de sífilis, e não solicitou que através de um consentimento voluntário, os sujeitos de pesquisa se dispusessem a participar do experimento, violando o Código de Nuremberg, de 1947, que determina que a participação de um ser humano em pesquisas médicas só poderá ocorrer mediante consentimento voluntário dos sujeitos da pesquisa (REVERBY; FOSTER, 2010).'
     }
 ]
+
+const header_titulo = `<strong>GALERIA DE ARTE INTERATIVA</strong>
+                        <br>
+                        <span>SÍFILIS: A “GRANDE IMITADORA” SOB O OLHAR DAS ARTES ATRAVÉS DOS SÉCULOS</span>`
 
 $('.obra').parent().hide();
 
@@ -47,6 +56,7 @@ $('.img_btn').click(function () {
     );
     $('.galeria').parent().hide();
     $('.obra').parent().show();
+    $('.header h2').html(obra.obra_titulo);
 })
 
 $('.botoes').on('show.bs.collapse','.collapse', function() {
@@ -56,4 +66,5 @@ $('.botoes').on('show.bs.collapse','.collapse', function() {
 $('.btn_voltar').click(function () {
     $('.galeria').parent().show();
     $('.obra').parent().hide();
+    $('.header h2').html(header_titulo);
 })
